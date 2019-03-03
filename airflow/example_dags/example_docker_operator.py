45f2709a42063d0e95553bde2b4ff26fe84495a2
Antoine Gervais <antoine.gervais@ville.montreal.qc.ca>
2019-03-02 20:31:06 -0500

    Fix the example for example_docker_operator

--- airflow/example_dags/example_docker_operator.py
+++ airflow/example_dags/example_docker_operator.py
@@ -47,15 +47,17 @@ t2 = BashOperator(
     retries=3,
     dag=dag)
 
-
-# t3 = DockerOperator(api_version='1.19',
-#     docker_url='tcp://localhost:2375', #Set your docker URL
-#     command='/bin/sleep 30',
-#     image='centos:latest',
-#     network_mode='bridge',
-#     task_id='docker_op_tester',
-#     dag=dag)
-
+t3 = DockerOperator(api_version='1.19',
+    docker_url='tcp://localhost:2375', #Set your docker URL
+    container_config={
+        'command':'/bin/sleep 30',
+        'image':'centos:latest',
+    },
+    host_config={
+        'network_mode': 'bridge',
+    },
+    task_id='docker_op_tester',
+    dag=dag)
 
 t4 = BashOperator(
     task_id='print_hello',
